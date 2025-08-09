2e2d31c30f24dfd0eec74965ae5c443f141256ed
Bohong Huang <bohonghuang@qq.com>
2025-08-09 23:34:00 +0800

    Implement `org-srs-stats-history-due-items`

--- org-srs-stats-history.el
+++ org-srs-stats-history.el
@@ -51,35 +51,50 @@
     (org-srs-query
      `(and (not (or suspended new))
            ,(lambda ()
-              (org-srs-table-goto-starred-line)
               (cl-loop with columns = (org-srs-table-column-name-number-alist)
                        with marker = (point-marker)
-                       initially (forward-line -1)
+                       initially (org-srs-table-goto-starred-line)
                        for previous-line = (cons nil nil) then line
                        for line = (cl-acons 'marker marker (org-srs-table-current-line columns))
                        for timestamp = (alist-get 'timestamp line)
                        for date = (org-srs-timestamp-date timestamp)
-                       while (alist-get 'rating line)
+                       while (or (null (car previous-line)) (alist-get 'rating line))
                        do (push (setf (cdr (last previous-line)) line) (gethash date table))
                        until (cl-minusp (forward-line -1))
                        until (org-at-table-hline-p))))
      source)
     table))
 
-(cl-defun org-srs-stats-history (source &optional (length (truncate (- (window-width) (* 5 2)) 3)))
+(defun org-srs-stats-history-review-record-p (record)
+  "Return non-nil if RECORD is a review record."
+  (cl-loop for (key . value) in record
+           count (eq key 'marker) into marker-count
+           when (eq key 'rating) return (< 0 marker-count 2)))
+
+(cl-defun org-srs-stats-history (source
+                                 &key
+                                 (length (truncate (- (window-width) (* 5 2)) 3))
+                                 (predicate #'org-srs-stats-history-review-record-p)
+                                 (increment -1))
   "Return recent review history data from SOURCE for visualization.
 
+SOURCE specifies the review data source to query.
 LENGTH specifies how many days of history to return, defaulting to a width
 suitable for window display.
-SOURCE specifies the review data source to query."
+PREDICATE is a function used to filter review records.
+INCREMENT determines the step between dates."
   (cl-loop with table = (org-srs-stats-history-full source)
            with labels and values
-           for timestamp = (org-srs-timestamp-now) then (org-srs-timestamp+ timestamp -1 :day)
+           initially (cl-assert (not (zerop increment)))
+           for timestamp = (org-srs-timestamp-now) then (org-srs-timestamp+ timestamp increment :day)
            for date = (org-srs-timestamp-date timestamp)
-           for reviews = (gethash date table)
+           for reviews = (cl-delete-if-not predicate (gethash date table))
            repeat length
            do (push (substring date 5) labels) (push reviews values)
-           finally (cl-return (cl-values labels values))))
+           finally (cl-return
+                    (if (cl-plusp increment)
+                        (cl-values (nreverse labels) (nreverse values))
+                      (cl-values labels values)))))
 
 ;;;###autoload
 (defun org-srs-stats-history-reviews (source)
@@ -122,6 +137,14 @@ SOURCE specifies the review data source to query."
                                                                        finally (cl-return (/ (* success-count 100) (+ success-count failure-count))))
                                                             (arith-error 0)))))
                        "Retention")))
+
+;;;###autoload
+(defun org-srs-stats-history-due-items (source)
+  "Display a chart of the count of due items per date for the given SOURCE."
+  (interactive (list (org-srs-review-source-dwim)))
+  (cl-flet ((cl-complement (predicate) (lambda (&rest args) (not (apply predicate args)))))
+    (cl-multiple-value-bind (labels values) (org-srs-stats-history source :predicate (cl-complement #'org-srs-stats-history-review-record-p) :increment 1)
+      (chart-bar-quickie 'vertical "Org-srs Statistics: Due Items" labels "Date" (mapcar #'length values) "Items"))))
 
 (provide 'org-srs-stats-history)
 ;;; org-srs-stats-history.el ends here
